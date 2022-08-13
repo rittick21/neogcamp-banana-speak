2e596048ece21c024496fe4af581bf2a051a0442
Rittick Roy <96196438+rittick21@users.noreply.github.com>
2022-08-14 03:45:30 +0530

    complete

--- app.js
+++ app.js
@@ -3,8 +3,28 @@
 let transLatebtn=document.querySelector('#translate-btn');
 let inputText=document.querySelector('#text-ip');
 let outputDiv=document.querySelector('#output');
+let serverURL="https://api.funtranslations.com/translate/minion.json";
+
+//to call the server as a function
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text;
+}
+//error handle function
+function errorHandel(error){
+    console.log("error occures",error);
+    alert("some thing wrong with server: Try again after some time");
+}
+
+//to fetch the server and display the content
 function textHandle(){
-    outputDiv.innerText="Written input: "+inputText.value;
+    const textIP=inputText.value;
+    fetch(getTranslationURL(textIP))
+    .then(response=>response.json())
+    .then(json=>{
+        let transLatedText=json.contents.translated;
+        outputDiv.innerText=transLatedText;
+        // console.log(json.contents.translated))
+    }).catch(errorHandel)
 };
 transLatebtn.addEventListener('click',textHandle);
 // transLatebtn.style.backgroundColor="red";
